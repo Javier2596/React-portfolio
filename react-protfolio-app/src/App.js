d7e62e6fed768736d231cc4147cc7c9f8cc8bc0f
Chito <Javier.martinez632@gmail.com>
2023-02-13 16:11:27 -0700

    created pages folder with files for home, portfolio, contacts, resume, aboutme/ created app.js, index.js, added styles.css folder

---
+++ react-protfolio-app/src/App.js
@@ -0,0 +1,8 @@
+// import necessary components and tools
+import React from 'react';
+import PortfolioContainer from './components/PortfolioContainer';
+
+// app function to organize components in correct manner in div element
+const App = () => <PortfolioContainer />;
+// exporting app function here
+export default App;
---
+++ react-protfolio-app/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hello, my name is Javier Martinez. I was born in Tucson, AZ and have lived here my whole life. I originally went to school for Criminal Justice Studies; but have since made a career change to study Web Development and pursue a career in the Web Dev industry. So far I have learned many differnt languages, databases, and libraries to increase my knowledge and better my career as a fullstack web developer. My goals are to continue learning and practicing everything I have learned in class so that I am comfortable applying to jobs in the industry; and to someday pass my knowledge and experiences to other web developers who are starting like me.   
+      </p>
+    </div>
+  );
+}
