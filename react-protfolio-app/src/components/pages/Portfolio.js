8c994b54beb1f6942ef8ed38d51f18e2c10fdc0d
Chito <Javier.martinez632@gmail.com>
2023-11-08 13:58:11 -0700

    updated css for portfolio page

--- react-protfolio-app/src/components/pages/Portfolio.js
+++ react-protfolio-app/src/components/pages/Portfolio.js
@@ -5,25 +5,27 @@ import '../../styles/index.css';
 
 export default function Portfolio() {
   return (
-  <div className="pb-5 p-4 ">
+  <div className="py-5 px-5">
   <h1>My Portfolio</h1>
-  
-    <div className="m-3 p-3">
-      <a href="https://mayamckinney.github.io/tumblr-marvel/">
-        <div>
-          <img src={img2} alt="Pokemon Search" className="images"></img>
-        </div>
-      </a>  
-      <p>A Front-end web Application that allows the user to search any Pokemon from any generation along with it's description and abilities as well as it's favorite hobby. This application demonstrates the basic front-end web development skills for most projects. My particular portion of this project was helping logic via JavaScript to help render the pokemon picture and decsription to the page. This project challenged me because it was my first real project that I worked on in a team setting. I did need help in the final days of working on the project because I could not get the description of the pokemon to render in the specific spot that I needed it to be. This project helped me understand that your first time coding is not going to be successful and that only time and practice will help me on my coding journey.</p>
-    </div>
+    <div className="my-5">
+      <div className="port">
+        <a href="https://mayamckinney.github.io/tumblr-marvel/">
+          <div className="gx-5">
+            <img src={img2} alt="Pokemon Search" className="images"></img>
+          </div>
+        </a>  
+        <p className="mx-5">A Front-end web Application that allows the user to search any Pokemon from any generation along with it's description and abilities as well as it's favorite hobby. This application demonstrates the basic front-end web development skills for most projects. My particular portion of this project was helping logic via JavaScript to help render the pokemon picture and decsription to the page. This project challenged me because it was my first real project that I worked on in a team setting. I did need help in the final days of working on the project because I could not get the description of the pokemon to render in the specific spot that I needed it to be. This project helped me understand that your first time coding is not going to be successful and that only time and practice will help me on my coding journey.</p>
+      </div>
 
-    <div className="m-3 p-3">
-      <a href="https://intentional-health.herokuapp.com/">
-        <div>
-          <img src={img3} alt="Intentional Health" className="images"></img>    
+      <div className="port my-5">
+        <a href="https://intentional-health.herokuapp.com/">
+          <img src={img3} alt="Intentional Health" className="images col"></img>    
+        
+        </a>  
+        <div className="mx-5">
+            <p className="container">This application is a fitness/ goal setter that helps the user keep track of their workout routines and what goals they wish to reach. To keep the application organized; it comes with a home page, work out page, and goal page. Before a user can start using the app, they have to create an account which will allow them to save their goals and workouts when they exit the application and return at a later time. This application is a full stack application that uses both Front-end (HTML, CSS framework: Bulma, Handlebar.js) and Back-end (MySQL2, Sequelize, Express.js, Bcrypt) technologies. My portion of the project was creating the api routes which allowed the user to navigate to specific pages, create data (goals and workouts) to the database, delete data, and pull up data that they created before. This project was challenging to me because it was my first project that I made using Back-end technologies I volunteered to work on the API routes because They seemed like the most challenging at the time and I had a difficult time understanding how they worked when I first learned about them. This project taught me that being a Fullstack web developer is not an easy task but that it is very rewarding when you can create something that works and functions like a real website.</p>
         </div>
-      </a>  
-      <p>This application is a fitness/ goal setter that helps the user keep track of their workout routines and what goals they wish to reach. To keep the application organized; it comes with a home page, work out page, and goal page. Before a user can start using the app, they have to create an account which will allow them to save their goals and workouts when they exit the application and return at a later time. This application is a full stack application that uses both Front-end (HTML, CSS framework: Bulma, Handlebar.js) and Back-end (MySQL2, Sequelize, Express.js, Bcrypt) technologies. My portion of the project was creating the api routes which allowed the user to navigate to specific pages, create data (goals and workouts) to the database, delete data, and pull up data that they created before. This project was challenging to me because it was my first project that I made using Back-end technologies I volunteered to work on the API routes because They seemed like the most challenging at the time and I had a difficult time understanding how they worked when I first learned about them. This project taught me that being a Fullstack web developer is not an easy task but that it is very rewarding when you can create something that works and functions like a real website.</p>
-    </div>
+      </div>
+    </div>  
 </div>
 )};
