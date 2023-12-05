68de4697f75188da6d4b8fe40525c6b505755b1a
Chito <Javier.martinez632@gmail.com>
2023-12-04 23:57:47 -0700

    added logic to notify users that their message was sent to the devs email

--- react-protfolio-app/src/components/pages/Contact.js
+++ react-protfolio-app/src/components/pages/Contact.js
@@ -1,43 +1,52 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-
   const form = useRef();
-
+  const [showMessage, setShowMessage] = useState(false);
+  
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.REACT_APP_SERVICE, process.env.REACT_APP_TEMPLATE, form.current, process.env.REACT_APP_USER)
-      .then((result) => {
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE, 
+        process.env.REACT_APP_TEMPLATE, 
+        form.current, 
+        process.env.REACT_APP_USER
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           console.log("message sent");
-      }, (error) => {
+          setShowMessage(true); //Shows message when email is sent
+        }, 
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
     <div className="p-4">
       <h1 className="title">Send me a Message</h1>
       <div>
-      <form ref={form} onSubmit={sendEmail}>
-        <div className="py-3">
-          <label className="contact">Name:</label>
-          <input type="text" name="user_name" className="form-control"/>
-        </div>
-        <div className='py-3'>  
-          <label className="contact">Email:</label>
-          <input type="email" name="user_email" className="form-control"/>
-        </div>
-        <div className='py-3'>
-          <label className="contact">Message:</label>
-          <textarea name="message" className="form-control" id="exampleFormControlTextarea1"/>
-        </div>
-          <input type="submit" value="Send" className="btn btn-outline-danger"/>  
-      </form>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="py-3">
+            <label className="contact">Name:</label>
+            <input type="text" name="user_name" className="form-control"/>
+          </div>
+          <div className='py-3'>  
+            <label className="contact">Email:</label>
+            <input type="email" name="user_email" className="form-control"/>
+          </div>
+          <div className='py-3'>
+            <label className="contact">Message:</label>
+            <textarea name="message" className="form-control" id="exampleFormControlTextarea1"/>
+          </div>
+            <input type="submit" value="Send" className="btn btn-outline-danger"/> {showMessage && <p className="secti">Message Sent!</p>}
+        </form>
       </div>   
-
     </div>
   );
-}
+};
