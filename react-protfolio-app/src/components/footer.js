4ef8c1f9fe73782d4e9c82ffe99ba0632cdccf2c
Chito <Javier.martinez632@gmail.com>
2023-12-10 09:28:24 -0700

    added links to footer icons/ styled color change

--- react-protfolio-app/src/components/footer.js
+++ react-protfolio-app/src/components/footer.js
@@ -10,10 +10,22 @@ function Footer() {
       <footer className="foot text-center">
         <h1>
           
-          <DiGithubBadge className="badges"/>
-          <FaLinkedin className="badges"/>
-          <FaInstagram className="badges"/>
-          <FaFacebookSquare className="badges"/>
+          
+            <a href="https://github.com/Javier2596">
+              <DiGithubBadge className="badges"/>
+            </a>
+
+            <a href="https://www.linkedin.com/in/javier-martinez-516933257/">
+              <FaLinkedin className="badges"/>
+            </a>
+
+            <a href="https://www.instagram.com/java_duh_hutt/">
+              <FaInstagram className="badges"/>
+            </a>
+
+            <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=100041542161659">
+              <FaFacebookSquare className="badges"/>
+            </a>
 
         </h1>
       </footer>
